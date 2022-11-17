67dc6e13dab87820c76ab902eb73211a2955736c
TheRuffian <7388720+bugpz@user.noreply.gitee.com>
2022-11-17 14:15:25 +0800

    feat: Pages

---
+++ src/main/java/org/theRuffian/pages/csdnPage.java
@@ -0,0 +1,9 @@
+package org.theRuffian.pages;
+
+import org.openqa.selenium.By;
+
+public class csdnPage {
+    public static By nickPic = By.cssSelector(".toolbar-btn.toolbar-btn-login.csdn-toolbar-fl.toolbar-subMenu-box");
+    public static By nickName = By.className("csdn-profile-nickName");
+    public static String url = "https://csdn.net";
+}
---
+++ src/main/java/org/theRuffian/pages/homePage.java
@@ -0,0 +1,11 @@
+package org.theRuffian.pages;
+
+import org.openqa.selenium.By;
+
+public class homePage {
+    public static By searchClick = By.id("su");
+    public static By searchText = By.id("kw");
+    public static String url = "https://www.baidu.com";
+
+
+}
